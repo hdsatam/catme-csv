c50fa182c7b9d7d970f063d8deabaac047230f67
Abigail Fox <abigailfireflyfox@gmail.com>
2020-10-05 12:39:49 -0400

    first commit

---
+++ catme.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import numpy as np
+import csv
+
+def process_csv(filename):
+    with open(filename) as f:
+        reader = csv.reader(f, delimiter=',')
+        # gets names of features
+        row1 = next(reader)
+        for row in reader:
+            if row[4] == '':
+                continue
+            #if row[4] == 'Total Attempts':
+                #continue
+            if ',' in row[4]:
+                continue
+            if '.' in row[4]:
+                continue
+            if int(row[4]) >= 16 and int(row[4]) <= 24:
+                print(row[2])
+    
+process_csv("catme.csv")
